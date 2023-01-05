f8bf486f9629b13703aaecc28d77288f1ec9d175
Theodoros Koxanoglou <theokoxan@gmail.com>
2023-01-05 11:53:42 +0200

    dfgh

--- Forms/Functions/LightsForm.Designer.cs
+++ Forms/Functions/LightsForm.Designer.cs
@@ -28,14 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LightsForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::hotel_app.Properties.Resources.lightbulb_on;
             this.pictureBox1.Location = new System.Drawing.Point(295, 119);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(256, 256);
