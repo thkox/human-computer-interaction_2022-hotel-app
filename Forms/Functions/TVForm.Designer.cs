4c1a881e692e1a814618e3899c0a97ff4bd6b077
Theodoros Koxanoglou <theokoxan@gmail.com>
2023-01-05 20:44:59 +0200

    tv layout almost ready

--- Forms/Functions/TVForm.Designer.cs
+++ Forms/Functions/TVForm.Designer.cs
@@ -28,19 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.volumeDownButton = new FontAwesome.Sharp.IconButton();
+            this.volumeUpButton = new FontAwesome.Sharp.IconButton();
+            this.powerButton = new FontAwesome.Sharp.IconButton();
+            this.backButton = new FontAwesome.Sharp.IconButton();
+            this.homeButton = new FontAwesome.Sharp.IconButton();
+            this.rightButton = new FontAwesome.Sharp.IconButton();
+            this.leftButton = new FontAwesome.Sharp.IconButton();
+            this.downButton = new FontAwesome.Sharp.IconButton();
+            this.upButton = new FontAwesome.Sharp.IconButton();
+            this.userPictureBox = new FontAwesome.Sharp.IconPictureBox();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.wathLaterLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // volumeDownButton
+            // 
+            this.volumeDownButton.IconChar = FontAwesome.Sharp.IconChar.VolumeLow;
+            this.volumeDownButton.IconColor = System.Drawing.Color.Black;
+            this.volumeDownButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.volumeDownButton.Location = new System.Drawing.Point(438, 540);
+            this.volumeDownButton.Name = "volumeDownButton";
+            this.volumeDownButton.Size = new System.Drawing.Size(75, 56);
+            this.volumeDownButton.TabIndex = 0;
+            this.volumeDownButton.UseVisualStyleBackColor = true;
+            // 
+            // volumeUpButton
+            // 
+            this.volumeUpButton.IconChar = FontAwesome.Sharp.IconChar.VolumeUp;
+            this.volumeUpButton.IconColor = System.Drawing.Color.Black;
+            this.volumeUpButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.volumeUpButton.Location = new System.Drawing.Point(438, 478);
+            this.volumeUpButton.Name = "volumeUpButton";
+            this.volumeUpButton.Size = new System.Drawing.Size(75, 56);
+            this.volumeUpButton.TabIndex = 1;
+            this.volumeUpButton.UseVisualStyleBackColor = true;
+            // 
+            // powerButton
+            // 
+            this.powerButton.IconChar = FontAwesome.Sharp.IconChar.PowerOff;
+            this.powerButton.IconColor = System.Drawing.Color.Black;
+            this.powerButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.powerButton.Location = new System.Drawing.Point(387, 406);
+            this.powerButton.Name = "powerButton";
+            this.powerButton.Size = new System.Drawing.Size(75, 56);
+            this.powerButton.TabIndex = 2;
+            this.powerButton.UseVisualStyleBackColor = true;
+            // 
+            // backButton
+            // 
+            this.backButton.IconChar = FontAwesome.Sharp.IconChar.ChevronCircleLeft;
+            this.backButton.IconColor = System.Drawing.Color.Black;
+            this.backButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.backButton.Location = new System.Drawing.Point(334, 478);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 56);
+            this.backButton.TabIndex = 3;
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // homeButton
+            // 
+            this.homeButton.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.homeButton.IconColor = System.Drawing.Color.Black;
+            this.homeButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.homeButton.Location = new System.Drawing.Point(334, 540);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(75, 56);
+            this.homeButton.TabIndex = 4;
+            this.homeButton.UseVisualStyleBackColor = true;
+            // 
+            // rightButton
+            // 
+            this.rightButton.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleRight;
+            this.rightButton.IconColor = System.Drawing.Color.Black;
+            this.rightButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.rightButton.Location = new System.Drawing.Point(686, 485);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(45, 48);
+            this.rightButton.TabIndex = 5;
+            this.rightButton.UseVisualStyleBackColor = true;
+            // 
+            // leftButton
+            // 
+            this.leftButton.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleLeft;
+            this.leftButton.IconColor = System.Drawing.Color.Black;
+            this.leftButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.leftButton.Location = new System.Drawing.Point(584, 485);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(45, 48);
+            this.leftButton.TabIndex = 6;
+            this.leftButton.UseVisualStyleBackColor = true;
+            // 
+            // downButton
+            // 
+            this.downButton.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleDown;
+            this.downButton.IconColor = System.Drawing.Color.Black;
+            this.downButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.downButton.Location = new System.Drawing.Point(635, 529);
+            this.downButton.Name = "downButton";
+            this.downButton.Size = new System.Drawing.Size(45, 48);
+            this.downButton.TabIndex = 7;
+            this.downButton.UseVisualStyleBackColor = true;
+            // 
+            // upButton
+            // 
+            this.upButton.IconChar = FontAwesome.Sharp.IconChar.CircleUp;
+            this.upButton.IconColor = System.Drawing.Color.Black;
+            this.upButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.upButton.Location = new System.Drawing.Point(635, 439);
+            this.upButton.Name = "upButton";
+            this.upButton.Size = new System.Drawing.Size(45, 48);
+            this.upButton.TabIndex = 8;
+            this.upButton.UseVisualStyleBackColor = true;
+            // 
+            // userPictureBox
+            // 
+            this.userPictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.userPictureBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.userPictureBox.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.userPictureBox.IconColor = System.Drawing.SystemColors.ControlText;
+            this.userPictureBox.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.userPictureBox.Location = new System.Drawing.Point(24, 30);
+            this.userPictureBox.Name = "userPictureBox";
+            this.userPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.userPictureBox.TabIndex = 9;
+            this.userPictureBox.TabStop = false;
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(62, 47);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(96, 15);
+            this.userNameLabel.TabIndex = 10;
+            this.userNameLabel.Text = "Username 007 😉";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(24, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1033, 253);
+            this.panel1.TabIndex = 11;
+            // 
+            // wathLaterLabel
+            // 
+            this.wathLaterLabel.AutoSize = true;
+            this.wathLaterLabel.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.wathLaterLabel.Location = new System.Drawing.Point(209, 74);
+            this.wathLaterLabel.Name = "wathLaterLabel";
+            this.wathLaterLabel.Size = new System.Drawing.Size(666, 35);
+            this.wathLaterLabel.TabIndex = 12;
+            this.wathLaterLabel.Text = "Recommended Movies and Shows of our Hotel Partners:";
             // 
             // TVForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.wathLaterLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.userPictureBox);
+            this.Controls.Add(this.upButton);
+            this.Controls.Add(this.downButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.powerButton);
+            this.Controls.Add(this.volumeUpButton);
+            this.Controls.Add(this.volumeDownButton);
             this.Name = "TVForm";
             this.Text = "TVForm";
+            ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private FontAwesome.Sharp.IconButton volumeDownButton;
+        private FontAwesome.Sharp.IconButton volumeUpButton;
+        private FontAwesome.Sharp.IconButton powerButton;
+        private FontAwesome.Sharp.IconButton backButton;
+        private FontAwesome.Sharp.IconButton homeButton;
+        private FontAwesome.Sharp.IconButton rightButton;
+        private FontAwesome.Sharp.IconButton leftButton;
+        private FontAwesome.Sharp.IconButton downButton;
+        private FontAwesome.Sharp.IconButton upButton;
+        private FontAwesome.Sharp.IconPictureBox userPictureBox;
+        private Label userNameLabel;
+        private Panel panel1;
+        private Label wathLaterLabel;
     }
 }
