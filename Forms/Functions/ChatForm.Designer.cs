d62ed6c01eeba08fef3a7929cffbd5537e2c423c
Apostolis2002 <apostolissiabanis@hotmail.com>
2023-02-04 20:11:43 +0200

    fix size for chat completed

--- Forms/Functions/ChatForm.Designer.cs
+++ Forms/Functions/ChatForm.Designer.cs
@@ -140,6 +140,8 @@
             this.Controls.Add(this.charBarPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "ChatForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chat Support";
